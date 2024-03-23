57d6863426ebe648e2e7e2fc98d2b3aca6400992
Abdurahmon55 <116945590+Abdurahmon55@users.noreply.github.com>
2024-03-23 23:58:02 +0500

    serializer

--- Backend/api/admin.py
+++ Backend/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Video)
+admin.site.register(Catigore)
+admin.site.register(Like)
+admin.site.register(VideoCommet)
+admin.site.register(Views)
+admin.site.register(replyedCommet)
+admin.site.register(CommetLike)
---
+++ Backend/api/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework.serializers import ModelSerializer
+from .models import *
+from django.contrib.auth import get_user_model
+
+class AuthSerializer(ModelSerializer):
+    class Meta:
+        model=get_user_model()
+        fields='__all__'
+
+def serializer(name, models):
+    class name(ModelSerializer):
+        class Meta:
+            model=models
+            fields='__all__'
+    return name 
+
+CommetLikeSeializer=serializer('CommetLikeSeializer', CommetLike)
+replyedCommetSerializer=serializer('replyedCommetSerializer', replyedCommet)
+ViewsSerializer=serializer('ViewsSerializer', Views)
+LikeSerializer=serializer('LikeSerializer', Like)
+CatigoreSerializer=serializer('CatigoreSerializer', Catigore)
+
+
+
+class VideoCommetSerializer(ModelSerializer):
+    replyeds=replyedCommetSerializer(many=True, read_only = True)
+    likes=CommetLikeSeializer(many=True, read_only = True)
+    class Meta:
+        model=VideoCommet
+        fields='__all__'
+
+class VideoSerializer(ModelSerializer):
+    catigores=CatigoreSerializer(many=True, read_only = True)
+    likes=LikeSerializer(many=True, read_only = True)
+    commets=VideoCommetSerializer(many=True, read_only = True)
+    views=ViewsSerializer(many=True, read_only = True)
+    class Meta:
+        model=Video
+        fields='__all__'
+
